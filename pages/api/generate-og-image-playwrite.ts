5b3867f3d2b98578ee87a2fa5d8d23984c63b682
Pratham <pratham.sharma2105@gmail.com>
2022-11-05 15:24:23 +0530

    puppeteer removed and playwrite added

---
+++ pages/api/generate-og-image-playwrite.ts
@@ -0,0 +1,30 @@
+import playwright from "playwright";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { title, desc, imageURL, price, discPrice } = req.query;
+  const url = `https://og-image-nextjs.vercel.app?title=${title}&desc=${desc}&price=${price}&discPrice=${discPrice}&imageURL=${imageURL}`;
+
+  try {
+    const browser = await playwright.chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    // await context.route("**.jpg", (route) => route.abort());
+    await page.goto(url);
+
+    const result = await page.screenshot({ fullPage: true });
+
+    // res.json({ status: true, result: result.toString("base64") });
+    res.setHeader("Content-Type", "image/png");
+    res.send(result);
+
+    await context.close();
+    await browser.close();
+  } catch (error) {
+    res.json({ status: false, error });
+  }
+}
