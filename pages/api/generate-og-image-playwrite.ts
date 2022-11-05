f56c52729cb6dedd2743fde51abc0458acc726cb
Pratham <pratham.sharma2105@gmail.com>
2022-11-05 15:49:53 +0530

    added playwrite aws lambda for production

--- pages/api/generate-og-image-playwrite.ts
+++ pages/api/generate-og-image-playwrite.ts
@@ -1,4 +1,4 @@
-import playwright from "playwright";
+// import playwright from "playwright-aws-lambda";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -8,23 +8,49 @@ export default async function handler(
   const { title, desc, imageURL, price, discPrice } = req.query;
   const url = `https://og-image-nextjs.vercel.app?title=${title}&desc=${desc}&price=${price}&discPrice=${discPrice}&imageURL=${imageURL}`;
 
-  try {
-    const browser = await playwright.chromium.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  if (process.env.PRODUCTION == "false") {
+    const playwright = require("playwright");
+    try {
+      const browser = await playwright.chromium.launch();
+      const context = await browser.newContext();
+      const page = await context.newPage();
 
-    // await context.route("**.jpg", (route) => route.abort());
-    await page.goto(url);
+      // await context.route("**.jpg", (route) => route.abort());
+      await page.goto(url);
 
-    const result = await page.screenshot({ fullPage: true });
+      const result = await page.screenshot({ fullPage: true });
 
-    // res.json({ status: true, result: result.toString("base64") });
-    res.setHeader("Content-Type", "image/png");
-    res.send(result);
+      // res.json({ status: true, result: result.toString("base64") });
+      res.setHeader("Content-Type", "image/png");
+      res.send(result);
 
-    await context.close();
-    await browser.close();
-  } catch (error) {
-    res.json({ status: false, error });
+      await context.close();
+      await browser.close();
+    } catch (error) {
+      res.json({ status: false, error });
+    }
+  } else {
+    const playwright = require("playwright-aws-lambda");
+    let browser = null;
+
+    try {
+      browser = await playwright.launchChromium();
+      const context = await browser.newContext();
+
+      const page = await context.newPage();
+      await page.goto(url);
+
+      const result = await page.screenshot({ fullPage: true });
+
+      // res.json({ status: true, result: result.toString("base64") });
+      res.setHeader("Content-Type", "image/png");
+      res.send(result);
+    } catch (error) {
+      throw error;
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
   }
 }
