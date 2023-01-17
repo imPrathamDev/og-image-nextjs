0050bdb54efd27c96158311fea54c36649c81256
Pratham <pratham.sharma2105@gmail.com>
2023-01-17 20:12:34 +0530

    üpdated unicoins

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,9 +59,13 @@ export default function Home({
             <p className="max-w-2xl my-6">{desc}</p>
 
             <div className="flex gap-x-2 items-baseline font-Cinzel font-semibold">
-              <span className="text-primary text-4xl">&#8377;{discPrice}</span>
+              <span className="text-primary text-4xl">
+                {`\u{20b9}`}
+                {discPrice}
+              </span>
               <span className="text-3xl text-gray-600 line-through">
-                &#8377;{price}
+                {`\u{20b9}`}
+                {price}
               </span>
             </div>
 
